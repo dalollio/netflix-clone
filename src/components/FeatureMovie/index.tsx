e42e893525844efae45f2a05c1119c9b65e8828c
Carlos Dalollio <dalollio.c@gmail.com>
2022-03-02 10:48:49 -0300

    :art: FeatureMovie mockup

---
+++ src/components/FeatureMovie/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Button from '../Button';
+import './style.css';
+// import { Container } from './styles';
+
+const FeatureMovie: React.FC = () => {
+  return (
+      <div className="container-detach">
+          <div className='container-fade'>
+          {/* <span>Efeito Borboleta</span> */}
+            <div style={{width:'40%'}}>
+                <div className='title'>Efeito Borboleta</div>
+                <div className='description'>O estudante universitário Evan Treborn, que vive assombrado por memórias trágicas, descobre um jeito de mudar o passado</div>
+                <div className='buttons'>
+                    <Button/>
+                    <Button/>
+                </div>
+            </div>
+          </div>
+      </div>
+  )
+}
+
+export default FeatureMovie;
