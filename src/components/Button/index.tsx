33300f05b55225ce22807e752317c7486ca093c4
Carlos Dalollio <dalollio.c@gmail.com>
2022-03-02 10:49:00 -0300

    :art: Pre build Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css'
+// import { Container } from './styles';
+
+const Button: React.FC = () => {
+  return (
+      <div className='button'>
+          <span>Assistir</span>
+      </div>
+  )
+}
+
+export default Button;
